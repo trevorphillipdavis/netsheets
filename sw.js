1606b946c329a1b1005c71147dad338e1316c088
trevorphillipdavis <trevorphillipdavis@gmail.com>
2026-07-05 12:51:52 -0400

    Bump cache to v1.1.1

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache version: bump CACHE_VERSION when files change
    ===================================================== */
 
-const CACHE_VERSION = "netsheets-v1.1.0";
+const CACHE_VERSION = "netsheets-v1.1.1";
 
 const APP_SHELL = [
   "./index.html",
